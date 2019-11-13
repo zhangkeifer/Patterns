049d900a88397a53812d4da350e5e70bdd46d33c
zhangkeifer <297873096@qq.com>
2019-11-13 17:08:47 +0800

    添加描述

--- src/main/java/com/FlammulinaBlog/单例模式01/A05Singleton.java
+++ src/main/java/com/FlammulinaBlog/单例模式01/A05Singleton.java
@@ -3,6 +3,10 @@ package com.FlammulinaBlog.单例模式01;
  * 双层检查锁定   + volatile
  * @author zhangkaifu
  * 有风险
+ * 似乎看起来降低了直接synchronized修饰类的开销，但是存在了多线程不安全。
+首先，当线程A第一次执行到//2的时候，线程B同时也执行//1，这个时候线程A可能会得到instance！= null的情况，然后返回该实例，但返回的是一个空的实例，
+是因为真正的情况线程A还没有初始化完毕instance实例。
+请参考：https://www.jianshu.com/p/17ed6a46ed85
  */
 public class A05Singleton {
 
@@ -12,10 +16,10 @@ public class A05Singleton {
 	}
 	
 	public static A05Singleton getInstance(){
-		if(instance == null){
+		if(instance == null){  //1
 			synchronized (A05Singleton.class) {
 				if(instance == null){
-					instance = new A05Singleton();
+					instance = new A05Singleton();//2
 				}
 			}
 		}
